c8108a4825a0768e69bf3bbda20de042b2dd7156
DanielRataj1 <127019047+DanielRataj1@users.noreply.github.com>
2024-05-05 17:29:53 +0200

    Update |--main.tf

--- |--main.tf
+++ |--main.tf
@@ -2,3 +2,9 @@ resource "local_file" "hello_world" {
   content  = "h = "Hello world" print(h)"
   filename = "${path.module}/hello_world.py"
 }
+
+terraform {
+  backend "local" {
+    path = "./terraform.tfstate"
+  }
+}
